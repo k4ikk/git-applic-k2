6540810dd68a6745559146d1a63af4cd0e0f84d4
Kaik Alves <kaik@duck.com>
2024-02-01 16:26:51 -0300

    0.0.3.4.6

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { Outlet } from "react-router-dom";
 import React from "react"
 import { Footer } from "./componets/footer";
+import './App.css'
 
 export function App() {
     return (
+
         <div id="root">
             <Outlet/> 
-            <Footer />
-      </div>    
+            <div id="footer"><Footer /></div>
+      </div> 
+         
     )
 }
--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,11 +1,28 @@
 import React from "react"
-import '../register/style.css'
-import { Dev } from "../dev"
+import '../login/style.css'
+import loginImg from '../../assets/loginImg.svg'
 
 export function Login() {
     return (
-        <>
-        <Dev />
-        </>
+        <body id="body-login">
+        <form id="login">
+            <div className="card-login">
+                <h1>Login</h1>
+                <p>Enter your datails</p>
+                <div className="login-entradas">
+                    <input type="email" placeholder="exemple@applic.co" />
+                    <input type="password" placeholder="Password"/>
+                    <a href="">Reset your password</a>
+                    <button>Login</button>
+                </div>
+                </div>
+            <div className="coluna-direita">
+                <img src={loginImg} alt="welcome" />
+                <h2 classname="h2-lebel">Welcome to Applic.co</h2>
+                <p>If you don't have an account?</p>
+                <a href="/register">Sign Up</a>
+            </div>
+        </form>
+        </body>
     )
 }
